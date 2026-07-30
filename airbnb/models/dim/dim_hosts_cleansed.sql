0b17e24188dd9b5891d3a99fc45b7496f969d406
Patrick Min <patrickwmin@gmail.com>
2026-07-30 16:44:06 -0400

    Migrate repo

---
+++ airbnb/models/dim/dim_hosts_cleansed.sql
@@ -0,0 +1,19 @@
+WITH src_hosts AS (
+    SELECT
+        *
+    FROM
+        {{ ref('src_hosts') }}
+)
+SELECT
+    host_id,
+    /*
+    Interesting; I was gonna use `COALESCE()`.
+    Ah, `NVL()` is a legacy, Oracle-specific function limited to two arguments,
+    whereas `COALESCE()` is a modern, ANSI-standard function.
+    */
+    NVL(host_name, 'Anonymous') AS host_name, 
+    is_superhost,
+    created_at,
+    updated_at
+FROM
+    src_hosts
---
+++ airbnb/models/dim/dim_listings_cleansed.sql
@@ -0,0 +1,27 @@
+WITH src_listings AS (
+    SELECT
+        *
+    FROM
+        /*
+        This is a Jinja template tag, and a way to reference the `src_listings` model.
+        dbt will substitute this with the view name.
+
+        Side note: Snowflake formats each double brace by inserting a space in between,
+        which is incorrect syntax and results in dbt raising an error.
+        */
+        {{ ref('src_listings') }}
+)
+SELECT
+    listing_id,
+    listing_name,
+    room_type,
+    CASE
+        WHEN minimum_nights = 0 THEN 1 -- Replace invalid values.
+        ELSE minimum_nights
+    END AS minimum_nights,
+    host_id,
+    REPLACE(price_str, '$')::NUMBER(10, 2) AS price,
+    created_at,
+    updated_at
+FROM
+    src_listings
